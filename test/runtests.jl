50299e61b5315e87ddc4247e8623e50f7666a02c
Brian Jackson <bjack205@gmail.com>
2021-02-25 15:59:04 -0500

    cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,12 @@
-using NBInclude
-@nbinclude(joinpath(@__DIR__, "..", "src", "Q1.ipynb"))
-@nbinclude(joinpath(@__DIR__, "..", "src", "Q2.ipynb"))
-@nbinclude(joinpath(@__DIR__, "..", "src", "Q3.ipynb"))
+module Q1
+    using NBInclude
+    @nbinclude(joinpath(@__DIR__, "..", "src", "Q1.ipynb"))
+end
+module Q2
+    using NBInclude
+    @nbinclude(joinpath(@__DIR__, "..", "src", "Q2.ipynb"))
+end
+module Q3
+    using NBInclude
+    @nbinclude(joinpath(@__DIR__, "..", "src", "Q3.ipynb"))
+end
