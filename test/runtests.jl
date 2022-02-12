5736a767cee951edd42320e2f10d82f7ee2d453e
Brian Jackson <bjack205@gmail.com>
2022-02-12 15:44:04 -0500

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,22 @@
-module Q1
-    using NBInclude
-    @nbinclude(joinpath(@__DIR__, "..", "src", "Q1.ipynb"))
+
+
+const NUMQUESTIONS = 3
+
+# Create a module for each question
+for i in 1:NUMQUESTIONS 
+@eval module $(Symbol("Q" * string(i)))
+include("autograder.jl")
+getname() = string(split(string(@__MODULE__), ".")[end])
+grade() = Autograder.gradequestion(getname()) 
+checktestsets(solutiondir=joinpath(@__DIR__, "..")) = Autograder.checktestsets(getname(), solutiondir)
 end
-module Q2
-    using NBInclude
-    @nbinclude(joinpath(@__DIR__, "..", "src", "Q2.ipynb"))
 end
-module Q3
-    using NBInclude
-    @nbinclude(joinpath(@__DIR__, "..", "src", "Q3.ipynb"))
+
+solutiondir = get(ARGS, 1, joinpath(@__DIR__, ".."))
+
+# Grade all of the questions
+modules = [@eval $(Symbol("Q" * string(i))) for i = 1:NUMQUESTIONS]
+results = map(modules) do mod
+    mod.checktestsets(solutiondir)
+    mod.grade()[1]
 end
